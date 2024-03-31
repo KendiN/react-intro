6c376d6c02b37ac9562b7ddf9afa7138de1c05d1
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-03-31 15:06:19 +0300

    feat: Displaying data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-function MyButton() {
+/*function MyButton() {
   return (
     <button>
       I'm a button
@@ -23,5 +23,28 @@ export default function MyApp() {
       <AboutPage />
       <img className = "avatar" />
     </div>
+  );
+}*/
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+    <h1>{user.name}</h1>
+    <img
+      className="avatar"
+      src={user.imageUrl}
+      alt={'Photo of ' + user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize
+      }}
+    />
+    </>
   );
 }
