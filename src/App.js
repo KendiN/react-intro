9217bdf4eeea13cb4c325b55837dee9807c6b32f
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-03-31 18:45:17 +0300

    feat: Rendering lists

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function MyApp() {
       <img className = "avatar" />
     </div>
   );
-}*/
+}
 
 const user = {
   name: 'Hedy Lamarr',
@@ -46,5 +46,28 @@ export default function Profile() {
       }}
     />
     </>
+  );
+}*/
+
+const products = [
+  {title: 'Cabbage', isFruit: false, id: 1 },
+  {title: 'Garlic', isFruit: false, id: 2 },
+  {title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+          color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li> 
+  );
+
+  return (
+    <ul>{listItems}</ul>
   );
 }
