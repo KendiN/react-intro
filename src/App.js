13f48c3ccfd75a37ca8d10083476bbb41843e8de
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-04-01 14:03:25 +0300

    feat: sharing data between components

--- src/App.js
+++ src/App.js
@@ -92,6 +92,7 @@ export default function MyApp() {
   );
 }*/
 
+/*counter that update separately
 import { useState } from 'react';
 
 export default function MyApp() {
@@ -116,4 +117,31 @@ return (
       Clicked {count} times
   </button>
  );
+} */
+
+//counters that update together
+import { useState } from 'react';
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick}/>
+      <MyButton count={count} onClick={handleClick}/>
+    </div>
+  );
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
 }
