de64412294df4efd99e07bfaf453d94ae1f7a0f2
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-04-02 17:27:04 +0300

    feat: passing data deeply with context

--- src/App.js
+++ src/App.js
@@ -305,7 +305,7 @@ function Panel({
   );
 }*/
 
-// Preserving and resetting state
+/* Preserving and resetting state
 import { useState } from 'react';
 import Chat from './Chat.js';
 import ContactList from './ContactList.js';
@@ -328,4 +328,35 @@ const contacts = [
   { name: 'Taylor', email: 'taylor@mail.com' },
   { name: 'Alice', email: 'alice@mail.com' },
   { name: 'Bob', email: 'bob@mail.com' }
-];
+];*/
+
+//Passing data deeply with context
+
+import Heading from './Heading.js';
+import Section from './Section.js';
+
+export default function Page() {
+  return (
+    <Section>
+      <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+
+
