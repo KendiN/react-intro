1dd7ab9118d4b4ecab929487c8abcecfdedeac6e
Kendi Njeru <kendi.njeru@strathmore.edu>
2024-04-02 16:47:29 +0300

    feat: preserving and resetting state

--- src/App.js
+++ src/App.js
@@ -259,7 +259,7 @@ export default function Form() {
   );
 }*/
 
-//Sharing state between components
+/*Sharing state between components
 import { useState } from 'react';
 
 export default function Accordion() {
@@ -303,4 +303,29 @@ function Panel({
       )}
     </section>
   );
+}*/
+
+// Preserving and resetting state
+import { useState } from 'react';
+import Chat from './Chat.js';
+import ContactList from './ContactList.js';
+
+export default function Messenger() {
+  const [to, setTo] = useState(contacts[0]);
+  return (
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedContact={to}
+        onSelect={contact => setTo(contact)}
+      />
+      <Chat contact={to} />
+    </div>
+  )
 }
+
+const contacts = [
+  { name: 'Taylor', email: 'taylor@mail.com' },
+  { name: 'Alice', email: 'alice@mail.com' },
+  { name: 'Bob', email: 'bob@mail.com' }
+];
